483ab3746180050c2f1d2ba4076a47e22c6b8705
José Miguel Rota <rota.jose@gmail.com>
2016-09-26 19:23:44 +0200

    * working with API now * reloading comments and notes by using an interval

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, URLSearchParams} from "@angular/http";
+import {Observable} from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'fountain-app',
@@ -10,38 +12,56 @@ import 'rxjs/add/operator/map';
 export class HelloComponent {
   public notes: any[] = [];
   public note: any = { dateTime: '12.12.1222' };
+  public loadError: boolean = false;
+  public loadingNotes: boolean = true;
 
-  private baseUrl = 'http://<BASE>/api';
+  private baseUrl = 'http://160.85.39.212:8080/api';
 
   constructor(private http: Http) {
+    Observable.interval(500)
+      .subscribe(() => {
+        if(!this.loadingNotes) {
+          this.loadNotes();
+        }
+      });
 
   }
 
   loadNotes() {
-    this.http.get(`${this.baseUrl}`)
+    this.loadingNotes = true;
+
+    this.http.get(`${this.baseUrl}/notes`)
       .map(res => res.json())
       .subscribe(res => {
+        this.loadError = false;
         this.notes = res.notes;
+        this.loadingNotes = false;
+      }, err => {
+        this.loadError = true
       });
   }
 
   saveNote(note: any): void {
-    note.id = Math.random();
-    this.notes.unshift(note);
 
-    this.http.post(`${this.baseUrl}/addNote`, JSON.stringify(note))
+    const query = new URLSearchParams();
+    query.set('subject', note.subject);
+    query.set('message', note.message);
+    query.set('creator', note.creator);
+
+    this.http.get(`${this.baseUrl}/addNote`, { search: query })
       .map(res => res.json())
       .subscribe(res => {
         note.id = res.id;
         this.notes.unshift(note);
         this.note = {};
-      });
+      }, err => alert('Unable to save note. Server unreachable'));
   }
 
-  deleteNote(note: any): void {
-    this.notes = this.notes.filter(n => n.id !== note.id);
+  deleteNote(note) {
+    const query = new URLSearchParams();
+    query.set('id', note.id);
 
-    this.http.post(`${this.baseUrl}/deleteNote`, note.id)
+    this.http.get(`${this.baseUrl}/deleteNote`, { search: query })
       .map(res => res.json())
       .subscribe(res => {
         this.notes = this.notes.filter(n => n.id !== note.id);
